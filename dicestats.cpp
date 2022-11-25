84e23af4fb54c8ac417ed7926ffd10058f1dcec3
Mud <lorenzo.cazzoli6@studio.unibo.it>
2022-11-25 13:33:52 +0100

    Corrected errors in graph, added bonus to the roll and comments

--- dicestats.cpp
+++ dicestats.cpp
@@ -7,22 +7,26 @@
 #include "TCanvas.h"
 #include "TH1I.h"
 
-
+// USING DECLARATIONS
 using std::vector;
 using std::cin;
 using std::cout;
 
-vector<int> roll_initializer(int n, int f, int m) {
+// NUMBER GENERATION WITH DEFAULT RANDOM ENGINE
+vector<int> roll_initializer(int n, int f, int m, int b) {
     
     std::default_random_engine generator;
+    //uniform distribution with range possible in dice roll
     std::uniform_int_distribution<int> distribution(1,f);
 
     vector<int> results;
+    //fillinfg the results vector with sum of rolls
     for(int i=0; i<m; i++) {
         results.push_back(0);
         for(int j=0; j<n; j++) {
             results[i] += distribution(generator);
         }
+        results[i] += b;
     }
 
     return results; 
@@ -30,25 +34,49 @@ vector<int> roll_initializer(int n, int f, int m) {
 
 void dicestats()
 {   
-    cout << "Number of dice per roll: ";
+    //------------------------------//
+    //      STARTING INTERFACE      //
+    //------------------------------//
+
+    cout << "Number of dices per roll: ";
+    //number of dices in each roll
     int n;
     cin >> n;
     cout << "Number of faces per dice: ";
+    //number of faces on each dice rolled
     int f;
     cin >> f;
     cout << "Number of rolls: ";
+    //number of rolls total
     int m;
     cin >> m;
+    cout << "Bonus to the total: ";
+    //int number to add to the total of each roll
+    int b;
+    cin >> b;
 
-    vector<int> dicevents = roll_initializer(n, f,m);
+    //vector containing the results
+    vector<int> dicevents = roll_initializer(n, f, m, b);
 
-    int bins = n*f;
-    double start = n;
-    double end = (n*f);
+    //------------------------------//
+    //   HISTOGRAM INIZIALIZATION   //
+    //------------------------------//
+
+    //number of bins in the histogram
+    int bins = n*f-(n-1);
+    //lowest value accepted in the histogram
+    double start = n+b-0.5;
+    //highest value accepted in the histogram
+    double end = (n*f)+b+0.5;
+    //inizialization of the histogram
     TH1 * h = new TH1I("h", "Result Distribution", bins, start, end);
+    //filling of the histogram with values from results vector
     for(auto i=dicevents.begin(); i!=dicevents.end(); i++) {
         h->Fill(*i);
     }
+
+    //inizialization of the canvas
     TCanvas *c = new TCanvas("Canvas");
+    //drawing the histogram on the canvas
     h->Draw("HIST");
 }
